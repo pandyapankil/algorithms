f936353494bbddd2cbf83a3ed0347a890d4902b3
Pankil <pandyapankil022@gmail.com>
2016-08-22 21:05:41 -0700

    isprime

---
+++ is_prime/ClientIsPrime.java
@@ -0,0 +1,6 @@
+public class ClientIsPrime{
+	public static void main(String[] args){
+		System.out.println(IsPrime.isPrime(16));
+		System.out.println(IsPrime.isPrime(13));		
+	}
+}
---
+++ is_prime/IsPrime.java
@@ -0,0 +1,8 @@
+public class IsPrime{
+	static boolean isPrime(int N){
+		if (N < 2) return false;
+		for (int i = 2; i * i <= N; i++)
+			if (N % i == 0) return false;
+		return true;
+	}
+}
