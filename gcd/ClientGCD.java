b656c0cd0e209e4ef5efd736394eb3313ba6f25e
pandyapankil <pandyapankil022@gmail.com>
2016-08-22 19:12:32 -0700

    gcd/euclidean

---
+++ gcd/ClientGCD.java
@@ -0,0 +1,5 @@
+public class ClientGCD{
+	public static void main(String[] args){
+		System.out.println(GCD.gcd(90,50));	
+	}
+}
---
+++ gcd/GCD.java
@@ -0,0 +1,7 @@
+public class GCD{
+	static int gcd(int p, int q){
+		if (q == 0) return p;
+		int r = p % q;
+		return gcd(q, r);	
+	}
+}
